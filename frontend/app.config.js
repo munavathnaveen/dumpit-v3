74e3f714f009b4b00c6f9d4084431e47467479a3
mdnihal5 <mdnihal.private1@gmail.com>
2025-06-05 10:20:23 +0530

    Remove native config from app.config.js for bare workflow

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,50 +1,18 @@
-const appJson = require('./_app.json');
+const appJson = require("./_app.json");
 const appConfig = appJson.expo;
 
 module.exports = {
-  ...appConfig,
-  name: "Dumpit",
-  slug: "dumpit",
-  orientation: "portrait",
-  icon: "./assets/logo.png",
-  userInterfaceStyle: "light",
-  newArchEnabled: true,
-  splash: {
-    image: "./assets/splash.png",
-    resizeMode: "contain",
-    backgroundColor: "#E23744"
-  },
-  scheme: "dumpit",
-  assetBundlePatterns: [
-    "**/*"
-  ],
-  ios: {
-    ...appConfig.ios,
-    supportsTablet: true,
-    config: {
-      googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
-    }
-  },
-  android: {
-    ...appConfig.android,
-    package: "com.mdnihal.dumpit",
-    adaptiveIcon: {
-      foregroundImage: "./assets/adaptive-icon.png",
-      backgroundColor: "#E23744"
+    ...appConfig,
+    name: "Dumpit",
+    slug: "dumpit",
+    assetBundlePatterns: ["**/*"],
+    web: {
+        favicon: "./assets/favicon.png",
     },
-    config: {
-      googleMaps: {
-        apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
-      }
-    }
-  },
-  web: {
-    favicon: "./assets/favicon.png"
-  },
-  extra: {
-    googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
-    eas: {
-      projectId: "0866d496-cd11-4928-87e5-ea9f960ed725"
-    }
-  }
-} 
+    extra: {
+        googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+        eas: {
+            projectId: "0866d496-cd11-4928-87e5-ea9f960ed725",
+        },
+    },
+};
